39f4deff4503d730c7ded5bd91a3af51883dd9ac
HosniHazem <hazem.hosni@aiesec.net>
2023-01-01 17:30:54 +0100

    express microservice

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const cors = require("cors");
 const express = require("express");
 const app = express();
-
+const PORT = process.env.PORT || 8086;
+const eurekaHelper = require('./eureka-helper');
 global.__basedir = __dirname;
 
 var corsOptions = {
@@ -15,7 +16,8 @@ const initRoutes = require("./src/routes");
 app.use(express.urlencoded({ extended: true }));
 initRoutes(app);
 
-let port = 8080;
+let port = 8086;
 app.listen(port, () => {
   console.log(`Running at localhost:${port}`);
 });
+eurekaHelper.registerWithEureka('UPLOAD-IMAGE', PORT);
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../controller/file.controller");
-
 let routes = (app) => {
   router.post("/upload", controller.upload);
   router.get("/files", controller.getListFiles);
@@ -10,5 +9,4 @@ let routes = (app) => {
 
   app.use(router);
 };
-
 module.exports = routes;
